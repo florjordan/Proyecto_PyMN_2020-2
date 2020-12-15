d20c30760be0eebf9d7ffcbf2f7288abe7f3aff9
Flor Jordan <75860636+florjordan@users.noreply.github.com>
2020-12-15 14:13:43 -0800

    Update 55.cpp

--- Practica 3/55.cpp
+++ Practica 3/55.cpp
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main() {
-long double qui, t1 = 0, t2 = 1, siguiente;
-int i, n;
-printf("ingrese el numero de terminos: ");
-scanf("%d", &n);
-for (i = 1; i <n; ++i) {
-/* printf("%.0Lf, ", t1);*/
-siguiente = t1 + t2;
-//a = t1;
-qui = t2/t1;
-t1 = t2;
-t2 = siguiente;
+ int i, n, r , nt, t1 = 0, t2 = 1;
+  printf("Secuencia Fibonacci \n")
+  printf("Ingresa el numero de terminos: \n");
+  scanf("%d",&n);
+  
+  for ( i = 1; i < n; ++i){
+    
+    nt= t1 + t2;
+    r = t2 / t1;
+    t1 = t2;
+    t2 = nt;
 }
-printf("el numero aureo es: %Lf\n",qui);
-return 0;
+  printf("Numero aureo correspondiente a los terminos: %d, \n", r);
+  
+  return0;
 }
 
