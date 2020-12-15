e23ffc18f2fe067bae29be43a09b479383e78460
Flor Jordan <75860636+florjordan@users.noreply.github.com>
2020-12-15 13:46:13 -0800

    Update 3.cpp

--- Practica 2/3.cpp
+++ Practica 2/3.cpp
@@ -1,17 +1,28 @@
-//La ecuaciÛn cuadr·tica//
+//La ecuaci√≥n cuadr√°tica//
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-
-for (int i=0;i < n;i++)
-{
-printf(ìIngrese elemento :î);
-scanf (ì %dî,&a[i]);
-printf(ì\nî);
+  int a,b,c,disc,ra,r1,r2;
+  printf("Ingresa el valor de a \n");
+  scanf("%d", &a);
+  printf("Ingresa el valor de b \n");
+  scanf("%d", &b);
+  printf("Ingresa el valor de c \n");
+  scanf("%d", &c);
+  disc=(b*b)+(-4*a*c);
+  ra=(disc)^(1/2);
+  if(disc<0){
+    printf("error: las soluciones no son reales");
+  }
+  else{
+    r1=(-b+ra)/(2*a);
+    r2=(-b-ra)/(2*a);
+    printf("El valor de la primera raiz es: %d \n", r1);
+    printf("El valor de la segunda raiz es: %d \n", r2);
+   }
 }
-}
-
 
 
 
